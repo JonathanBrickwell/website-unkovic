9b539094ddf67012caf55a93d291799350a23966
fistrekkristijan <fistrekkristijan@gmail.com>
2022-06-02 22:27:03 +0200

    Initial Commit

---
+++ assets/js/preloader.js
@@ -0,0 +1,7 @@
+setTimeout(() => {
+    document.getElementById('_preloader').classList.remove('_show_preloader');
+}, 2500);
+
+setTimeout(() => {
+    document.getElementById('side-navbar').classList.remove('hidden');
+}, 4500);
